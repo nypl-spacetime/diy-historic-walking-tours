49210bfcb140cf0ecee6af8cf666402db513f205
bfreeds <brandynfriedly@gmail.com>
2017-10-18 07:50:18 -0400

    Add solution for instantiating Leaflet

---
+++ public/map.js
@@ -0,0 +1,5 @@
+// Initialize a Leaflet map and store a reference to the map Object
+var map = L.map('map')
+
+//  Set the [lat, long], and zoom for the map using Leaflet's setView method
+map.setView([40.80558317487379, -73.94968271255495], 16)
