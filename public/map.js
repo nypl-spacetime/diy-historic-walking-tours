5e63224bbc47b6cbc422a2b179a8ff5e88e007a5
bfreeds <brandynfriedly@gmail.com>
2017-10-18 21:35:27 -0400

    Add L.tileLayer() for NYPL basemap layer

--- public/map.js
+++ public/map.js
@@ -3,3 +3,9 @@ var map = L.map('map')
 
 //  Set the [lat, long], and zoom for the map using Leaflet's setView method
 map.setView([40.80558317487379, -73.94968271255495], 16)
+
+// Add a tile layer from OpenStreetMaps tile server
+// Pass a configuration object {} for attribution
+L.tileLayer('http://maps.nypl.org/warper/layers/tile/862/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.nypl.org/">New York Public Library</a>'
+}).addTo(map); // this .addTo() syntax is called "method chaining".  It performs a function from the result of the previous function - L.tileLayer()
